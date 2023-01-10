912954f4c0997254e763441a7976bc154f51973c
srpallab <pallab.alif@gmail.com>
2023-01-11 00:17:26 +0600

    Initial Comit

---
+++ late_in_dart/bin/late_in_dart.dart
@@ -0,0 +1,5 @@
+import 'package:late_in_dart/late_in_dart.dart' as late_in_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${late_in_dart.calculate()}!');
+}
---
+++ late_in_dart/test/late_in_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:late_in_dart/late_in_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
