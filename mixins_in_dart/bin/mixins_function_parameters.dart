e53db1b0a68e6cefd50ef3c52f5f2e331cb5126c
srpallab <pallab.alif@gmail.com>
2023-01-26 02:33:40 +0600

    ADD: Mixins Function.

--- mixins_in_dart/bin/mixins_function_parameters.dart
+++ mixins_in_dart/bin/mixins_function_parameters.dart
@@ -1,4 +1,34 @@
 void main() {
+  Person shafiqur = Person(firstName: "Shafiqur", lastName: "Rahman");
+
+  print(shafiqur.fullName);
+
+  print(getFullName(shafiqur));
   
+}
+
+String getFullName(HasFullName obj) => obj.fullName;
+
+
+mixin HasFirstName {
+  String get firstName;
+}
+
+mixin HasLastName {
+  String get lastName;
+}
+
+mixin HasFullName on HasFirstName, HasLastName {
+  String get fullName => "$firstName $lastName";
+}
+
+class Person with HasFirstName, HasLastName, HasFullName {
+  @override
+  final String firstName;
+
+  @override
+  final String lastName;
+  
+  Person({required this.firstName, required this.lastName});
 }
 
